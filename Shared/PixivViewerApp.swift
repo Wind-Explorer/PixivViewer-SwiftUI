36b9adfcc099d83605fbf968692847eeed118ccf
ohaiibuzzle <23693150+ohaiibuzzle@users.noreply.github.com>
2022-06-26 21:21:02 +0700

    Initial Commit

---
+++ Shared/PixivViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PixivViewerApp.swift
+//  Shared
+//
+//  Created by Venti on 26/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct PixivViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
